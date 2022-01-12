d2ecdd5fa200d4a89856337666e4161b8a4b00fc
Titus Njehia <titusnjehiaaa@gmail.com>
2022-01-12 15:43:38 +0300

    add sprinter file

---
+++ ch_04/sprinter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# sprinter.py
+
+# 100 m is 0.1 km
+
+distance = 0.1
+
+# 9.87 s is 9.87/60*60 h
+
+time = 9.87 / 3600
+
+speed = distance / time
+
+print("The average speed of a sprinter is {0} km/h".format(speed))
